b8d4d9dc4483c7477f850d58c9f8cb8f0da0c240
Xiang Wu <xiang.wu@joingsg.com>
2022-07-12 14:37:44 +0200

    add custom theme screen size

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail"
 function Results(props) {
     const result = props.results
     return (
-        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
             <Thumbnail key={result.id} result={result} />
         </div>
     )
