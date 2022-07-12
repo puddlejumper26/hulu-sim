293394d018d66987aca182db43a4dcfbc1ab7a6f
Xiang Wu <xiang.wu@joingsg.com>
2022-07-12 14:22:28 +0200

    sm md xl to result.js

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail"
 function Results(props) {
     const result = props.results
     return (
-        <div>
+        <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
             <Thumbnail key={result.id} result={result} />
         </div>
     )
